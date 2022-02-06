89e6300691a0c0b3a7851299c755867a6519a7e3
Alexis Henriot <alexis42henriot@gmail.com>
2022-02-06 09:55:47 +0100

    Fixing clipsgrid size bug

--- Source/Graphics/Components/timeline.cpp
+++ Source/Graphics/Components/timeline.cpp
@@ -146,6 +146,9 @@ int Timeline::getClipsGridWidth() const
 void Timeline::resizeEvent(QResizeEvent *)
 {
     mMainLayout.setGeometry(QRect(0, 0, width(), height()));
+
+    mClipsGrid->setGeometry(mClipsGrid->x(), 0, mClipsGridViewport.width(), mClipsGridViewport.height());
+    mDivisionsMarker->setGeometry(mDivisionsMarker->x(), 0, mDivisionsMarkerViewport.width(), mDivisionsMarkerViewport.height());
 }
 
 void Timeline::paintEvent(QPaintEvent *)
