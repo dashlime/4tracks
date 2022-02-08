7d9265c7ec32cd7606e8b3e8bf188806e073450a
Alexis Henriot <alexis42henriot@gmail.com>
2022-02-08 17:26:17 +0100

    Adding mp3 and aiff support

--- Tests/Audio/audiofileloadertests.cpp
+++ Tests/Audio/audiofileloadertests.cpp
@@ -4,6 +4,7 @@ TEST_F(AudioFileLoaderTests, CheckFileName)
 {
     EXPECT_TRUE(mFileLoader->checkFileName("example file.wav"));
     EXPECT_TRUE(mFileLoader->checkFileName("example file.mp3"));
+    EXPECT_TRUE(mFileLoader->checkFileName("example file.aiff"));
     EXPECT_FALSE(mFileLoader->checkFileName("example file"));
     EXPECT_FALSE(mFileLoader->checkFileName("example file.txt"));
     EXPECT_FALSE(mFileLoader->checkFileName("example file."));
