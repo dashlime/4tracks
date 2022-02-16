629e54eefdc0034a5937d91c5538a697193833df
Alexis Henriot <alexis42henriot@gmail.com>
2022-02-16 11:22:53 +0100

    Cleaning up code

--- Tests/Graphics/selectionmanagertests.cpp
+++ Tests/Graphics/selectionmanagertests.cpp
@@ -2,13 +2,21 @@
 
 TEST_F(SelectionManagerTests, GenerateSelectionModifiers)
 {
-    auto *event = new QMouseEvent(QMouseEvent::MouseButtonPress, QPoint(), Qt::LeftButton, Qt::MouseButtons(), Qt::KeyboardModifiers());
+    auto *event = new QMouseEvent(QMouseEvent::MouseButtonPress,
+                                  QPoint(),
+                                  Qt::LeftButton,
+                                  Qt::MouseButtons(),
+                                  Qt::KeyboardModifiers());
     auto modifiers = Graphics::Selection::generateSelectionModifiers(event);
     EXPECT_FALSE(modifiers.testFlag(Graphics::Selection::CtrlModifier));
     EXPECT_FALSE(modifiers.testFlag(Graphics::Selection::ShiftModifier));
     EXPECT_FALSE(modifiers.testFlag(Graphics::Selection::RightButtonModifier));
 
-    event = new QMouseEvent(QMouseEvent::MouseButtonPress, QPoint(), Qt::LeftButton, Qt::MouseButtons(), Qt::KeyboardModifiers(Qt::ControlModifier));
+    event = new QMouseEvent(QMouseEvent::MouseButtonPress,
+                            QPoint(),
+                            Qt::LeftButton,
+                            Qt::MouseButtons(),
+                            Qt::KeyboardModifiers(Qt::ControlModifier));
     modifiers = Graphics::Selection::generateSelectionModifiers(event);
     EXPECT_TRUE(modifiers.testFlag(Graphics::Selection::CtrlModifier));
     EXPECT_FALSE(modifiers.testFlag(Graphics::Selection::ShiftModifier));
